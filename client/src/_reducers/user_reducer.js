8b4d01f012fedd937f262f6e5cd81c623a1962da
Helen <helentran.dev@gmail.com>
2020-11-11 12:49:01 -0500

    Add auth and logout action and reducers

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,15 +1,27 @@
-import { LOGIN_USER, REGISTER_USER } from '../actions/types';
+import {
+    LOGIN_USER, 
+    REGISTER_USER,
+    AUTH_USER,
+    LOGOUT_USER 
+} from '../_actions/types';
 
 export default function ( state={}, action ) {
     switch (action.type) {
+    
+        case REGISTER_USER:
+            return { ...state, loginSuccess: action.payload }
+        
         // If the action type is LOGIN_USER
         case LOGIN_USER:
             // Then we return everything we have in state and in loginSuccess which stores true or false in payload
             // The ...state means we will get everything in the state
             return { ...state, loginSuccess: action.payload }
 
-        case REGISTER_USER:
-            return { ...state, loginSuccess: action.payload }
+        case AUTH_USER:
+            return { ...state, userData: action.payload }
+
+        case LOGOUT_USER:
+            return { ...state, }
             
         default: 
             return state;
