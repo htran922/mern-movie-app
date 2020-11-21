11b20ad09531cd53c4e86be7a16278b02181b45d
Helen <helentran.dev@gmail.com>
2020-11-21 17:25:58 -0500

    Create landing page

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,10 @@
 // SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = '1cab77f3eaf7ebcaaa45659d451ce474';
+
+// We will GET movie by latest, detail, reviews, and trending... which have same beginning URL
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+// GET image same URL part
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import { FaCode } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react';
+import { API_URL, API_KEY, IMAGE_URL } from '../../Config';
+import { Typography, Row } from 'antd';
+import MainImage from './Sections/MainImage';
 
-function LandingPage(props) {
+const { Title } = Typography;
+
+function LandingPage() {
+
+    // Create state for fetched data from Movie DB API to be stored inside
+    const [Movies, setMovies] = useState([])
+
+    useEffect(() => {
+        // Get popular movies
+        // Source: https://developers.themoviedb.org/3/movies/get-popular-movies
+        fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+            .then(response => response.json())
+            .then(response => { 
+                console.log(response)
+                setMovies(response.results)
+            })
+
+    }, [])
+
     return (
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }}/>
-            <br/>
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width:'100%', margin: 0 }}>
+            {/* Movie Main Image Component */}
+            {/* This means that when we get the movie information, we will render the MainImage component */}
+            {Movies[1] && 
+                <MainImage 
+                    image={`${IMAGE_URL}w1280${Movies[1].backdrop_path && Movies[1].backdrop_path}`} 
+                    title={Movies[1].original_title} 
+                    text={Movies[1].overview}
+                />
+            }
+            
+            
+            {/* Body */}
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+                <Title level={2}>Latest Movies</Title>
+                <hr/>
+
+                {/* Grid Cards */}
+                <Row gutter={[16,16]} >
+
+                </Row>
+                {/* Load More Button*/}
+                <br/>
+                <div style={{display:'flex', justifyContent:'center'}}>
+                    <button onClick>Load More</button>
+                </div>
+            </div>
         </div>
     );
 }
