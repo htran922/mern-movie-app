230ab3dbbaf8d50862d5fe339dd9a722447d91e1
Helen <helentran.dev@gmail.com>
2020-11-26 17:31:24 -0500

    Revert changes to proxy setup

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,19 +1,12 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// module.exports = function(app) {
-//   app.use(
-//     '/api',
-//     createProxyMiddleware({
-//       target: 'http://localhost:5000',
-//       changeOrigin: true
-//     })
-//   );
-// };
-
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-
-    app.use(proxy("/api", { target: "http://localhost:5000/" }));
-
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true
+    })
+  );
 };
+
